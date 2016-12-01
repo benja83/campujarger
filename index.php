73a6ddc80c7d6cca75c878126d3baf6981742ace
benja83 <tarennebenjamin@gmail.com>
2016-12-01 19:01:45 +0100

    Herokurizes app

--- index.php
+++ index.php
@@ -5,4 +5,11 @@
  * @since         CakePHP(tm) v 0.10.0.1076
  */
 
-require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+define('APP_DIR', 'app');
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+define('WEBROOT_DIR', 'webroot');
+define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
+require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
+
+// require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
